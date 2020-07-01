0deb5a256ef4ac1da50f5b80bfc78029809737e3
John Falcón <johngrowscode@gmail.com>
2020-07-01 15:14:55 -0500

    change: we are not importing env settings for the moment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import ReactDOM from "react-dom";
 import App from "./App";
-import env from "settings/env";
 
 function ReactRoot() {
-
     return <App />;
 }
 
-ReactDOM.render(
-	<ReactRoot />,
-    document.getElementById("root"),
-);
+ReactDOM.render(<ReactRoot />, document.getElementById("root"));
